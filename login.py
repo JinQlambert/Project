49ee5445f0cdb09f384840683d37a1a9e8d5d472
JinQlambert <lambertfeast@gmail.com>
2018-01-12 16:45:11 +0900

    version0.0.1

--- login.py
+++ login.py
@@ -5,7 +5,6 @@
 
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
